1922f92161187919d0efe5c0c1dc4e1c2af2fc8e
Bill Dang <sparkfire.ffs@gmail.com>
2020-10-29 00:40:56 -0500

    Added ball catcher and return to dispenser system

---
+++ Assets/Scripts/Editor/BallDispenserEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BallDispenser), true)]
+public class BallDispenserEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        BallDispenser dispenser = target as BallDispenser;
+
+        if(GUILayout.Button("Spawn Test Ball")) {
+            dispenser.SpawnBall(dispenser.testBallIndex);
+        }
+        if(GUILayout.Button("Spawn Random Test Ball")) {
+            dispenser.SpawnBall(Random.Range(0, dispenser.ballList.Count - 1));
+        }
+        if(GUILayout.Button("Move Referenced Ball")) {
+            dispenser.MoveTestBall();
+        }
+    }
+
+}
