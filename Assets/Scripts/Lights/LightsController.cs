e28c67bb4bdcb1b9615c66792cb7c095a571f234
Bill Dang <sparkfire.ffs@gmail.com>
2020-10-30 18:01:40 -0500

    Update main and work on lights

--- Assets/Scripts/Lights/LightsController.cs
+++ Assets/Scripts/Lights/LightsController.cs
@@ -34,18 +34,32 @@ public class LightsController : MonoBehaviour {
         float length = 0.5f;
         int amount = 4;
         float delay = Random.Range(1f, 3f);
-        FlickerRandomLight(length, amount, delay);
+
+        if(GameStateController.GetCurrentFrame() % 2 == 0 && GameStateController.GetCurrentFrame() <= 6) { // 2, 4, 6 - flicker and turn off
+            TurnOffRandom(length, amount, delay, GameStateController.GetCurrentFrame() / 2);
+        } else // Odd - flicker
+            FlickerRandomLight(length, amount, delay);
     }
 
-    public void FlickerLight(int index, float length = 0.5f, int amount = 4, float delay = 0f) {
+    private void TurnOffRandom(float length, int amount, float delay, int column = -1) {
+        int index;
+        if(column == -1)
+            index = Random.Range(0, lights.Length);
+        else {
+            index = (Random.Range(0, 3) * 3) + column;
+        }
+        FlickerLight(index, length, amount, delay, true);
+    }
+
+    public void FlickerLight(int index, float length = 0.5f, int amount = 4, float delay = 0f, bool toggle = false, bool onlyIfOn = false) {
+        lights[index].FlickerLight(length, amount, delay, toggle, onlyIfOn);
+    }
+
+    public void FlickerRandomLight(float length = 0.5f, int amount = 4, float delay = 0f, bool toggle = false, bool onlyIfOn = false) {
         for(int i = 0; i < 4; i++) {
-            if(lights[index].FlickerLight(length, amount, delay))
+            if(lights[Random.Range(0, lights.Length)].FlickerLight(length, amount, delay, toggle, onlyIfOn))
                 break;
         }
-    }
-
-    public void FlickerRandomLight(float length = 0.5f, int amount = 4, float delay = 0f) {
-        FlickerLight(Random.Range(0, lights.Length), length, amount, delay);
     }
 
 }
