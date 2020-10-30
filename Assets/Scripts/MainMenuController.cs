cb9603bcb037af948e4b503940ca28d238182569
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-10-29 23:17:34 -0500

    Main Menu set up

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] GameObject start;
+    [SerializeField] GameObject controls;
+    [SerializeField] GameObject credits;
+    [SerializeField] GameObject quit;
+    [SerializeField] GameObject title;
+    [SerializeField] GameObject controlPage;
+    [SerializeField] GameObject creditPage;
+    private GameObject currentPage;
+
+    private void Start()
+    {
+        currentPage = title;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform.gameObject == start)
+                {
+                    print("start");
+                    SceneManager.LoadScene("_Main");
+                }
+                else if (hit.transform.gameObject == controls)
+                {
+                    print("controls");
+                    controlsPage();
+                }
+                else if (hit.transform.gameObject == credits)
+                {
+                    creditsPage();
+                    print("credit");
+                }
+                else if (hit.transform.gameObject == quit)
+                {
+                    print("quit");
+                    Application.Quit();
+                }
+            }
+        }
+    }
+
+    private void controlsPage()
+    {
+        currentPage.SetActive(false);
+        currentPage = controlPage;
+        currentPage.SetActive(true);
+    }
+
+    private void creditsPage()
+    {
+        currentPage.SetActive(false);
+        currentPage = creditPage;
+        currentPage.SetActive(true);
+    }
+}
