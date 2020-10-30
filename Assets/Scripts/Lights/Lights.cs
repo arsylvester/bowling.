07c222e88af983096224a32df7dfdae7968315f6
Bill Dang <sparkfire.ffs@gmail.com>
2020-10-30 14:54:34 -0500

    Add lights system

---
+++ Assets/Scripts/Lights/Lights.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour {
+
+    [SerializeField] private GameObject lightObj;
+    [SerializeField] private MeshRenderer materialObj;
+    [SerializeField] private Material onMaterial, offMaterial;
+    [SerializeField] private bool offByDefault;
+
+    private bool isFlickering;
+
+    private void Awake() {
+        if(offByDefault)
+            SetLight(false);
+        isFlickering = false;
+    }
+
+    public void SetLight(bool active) {
+        if(active == lightObj.activeSelf)
+            return;
+
+        lightObj.SetActive(active);
+        if(onMaterial && offMaterial) {
+            if(active)
+                materialObj.material = onMaterial;
+            else
+                materialObj.material = offMaterial;
+        }
+    }
+
+    public void ToggleLight() {
+        SetLight(!lightObj.activeSelf);
+    }
+
+    public bool IsLightOn() {
+        return lightObj.activeSelf;
+    }
+
+    public bool FlickerLight(float length, int amount, float delay = 0f) {
+        if(isFlickering)
+            return false;
+        StartCoroutine(Flicker(length, amount, delay));
+        return true;
+    }
+
+    private IEnumerator Flicker(float length, int amount, float delay = 0f) {
+        isFlickering = true;
+        yield return new WaitForSecondsRealtime(delay);
+
+        float pauseTime = length / (amount * 2);
+        for(int i = 0; i < amount; i++) {
+            yield return new WaitForSecondsRealtime(pauseTime);
+            ToggleLight();
+            yield return new WaitForSecondsRealtime(pauseTime);
+            ToggleLight();
+        }
+        isFlickering = false;
+    }
+
+}
---
+++ Assets/Scripts/Lights/LightsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsController : MonoBehaviour {
+
+    public Lights[] lights;
+
+    private void Awake() {
+        if(lights.Length == 0)
+            lights = FindObjectsOfType<Lights>();
+    }
+
+    private void Start() {
+        GameStateController._instance.m_NewFrame.AddListener(FlickerOnNextFrame);
+        StartCoroutine(FlickerRandomly());
+    }
+
+    private IEnumerator FlickerRandomly() {
+        float waitTime = 20f;
+        float length = 0.5f;
+        int amount = 4;
+        float delay = Random.Range(1f, 3f);
+
+        while(true) {
+            yield return new WaitForSecondsRealtime(waitTime);
+            FlickerRandomLight(length, amount, delay);
+        }
+    }
+
+    private void FlickerOnNextFrame() {
+        //float randomModifier = Random.Range(1, GameStateController.GetCurrentFrame() + 1);
+        float length = 0.5f;
+        int amount = 4;
+        float delay = Random.Range(1f, 3f);
+        FlickerRandomLight(length, amount, delay);
+    }
+
+    public void FlickerLight(int index, float length = 0.5f, int amount = 4, float delay = 0f) {
+        for(int i = 0; i < 4; i++) {
+            if(lights[index].FlickerLight(length, amount, delay))
+                break;
+        }
+    }
+
+    public void FlickerRandomLight(float length = 0.5f, int amount = 4, float delay = 0f) {
+        FlickerLight(Random.Range(0, lights.Length), length, amount, delay);
+    }
+
+}
