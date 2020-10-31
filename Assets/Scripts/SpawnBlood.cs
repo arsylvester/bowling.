a4ba5b4e992f8e59e47d2c8f4c3d37f4329b0819
Bill Dang <sparkfire.ffs@gmail.com>
2020-10-30 21:52:58 -0500

    Work on skull and blood spawners

---
+++ Assets/Scripts/SpawnBlood.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlood : MonoBehaviour {
+
+    [SerializeField] private List<GameObject> blood = new List<GameObject>();
+    [SerializeField] private int frameNumber;
+
+    private void Start() {
+        GameStateController._instance.m_NewFrame.AddListener(() => {
+            StartCoroutine(SpawnBloodFunc());
+        });
+    }
+
+    private IEnumerator SpawnBloodFunc() {
+        while(true) {
+            if(GameStateController.GetCurrentFrame() >= frameNumber && PlayerController.lookingAt == 2) { // Looking at score screen
+                foreach(GameObject obj in blood)
+                    obj.SetActive(true);
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+}
